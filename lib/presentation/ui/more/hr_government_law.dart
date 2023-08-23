01d3c059132b5b1a98f116d742eb45c3cb8f090e
sudheerkumar-sgd <sudheer.akula@sgd.uaqgov.ae>
2023-08-23 15:08:42 +0400

    #added hr law pdf

--- lib/presentation/ui/more/hr_government_law.dart
+++ lib/presentation/ui/more/hr_government_law.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:malomati/core/common/common.dart';
-import 'package:malomati/res/drawables/background_box_decoration.dart';
 import '../widgets/back_app_bar.dart';
+import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
 
 class HRGovernmentLaw extends StatelessWidget {
   static const String route = '/HRGovernmentLaw';
   const HRGovernmentLaw({super.key});
-
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -26,61 +25,11 @@ class HRGovernmentLaw extends StatelessWidget {
               SizedBox(
                 height: context.resources.dimen.dp20,
               ),
-              Container(
-                width: double.infinity,
-                padding: EdgeInsets.all(context.resources.dimen.dp15),
-                decoration: BackgroundBoxDecoration(
-                        boxColor: context.resources.color.colorWhite,
-                        radious: context.resources.dimen.dp10)
-                    .roundedCornerBox,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      context.string.hRGovernmentLaw,
-                      style: context.textFontWeight600
-                          .onFontSize(context.resources.dimen.dp15),
-                    ),
-                    SizedBox(
-                      height: context.resources.dimen.dp5,
-                    ),
-                    Text(
-                      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text',
-                      style: context.textFontWeight400
-                          .onFontSize(context.resources.dimen.dp11),
-                    ),
-                  ],
+              Expanded(
+                child: SfPdfViewer.asset(
+                  'assets/json/hr_law.pdf',
                 ),
               ),
-              SizedBox(
-                height: context.resources.dimen.dp20,
-              ),
-              Container(
-                width: double.infinity,
-                padding: EdgeInsets.all(context.resources.dimen.dp15),
-                decoration: BackgroundBoxDecoration(
-                        boxColor: context.resources.color.colorWhite,
-                        radious: context.resources.dimen.dp10)
-                    .roundedCornerBox,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Privacy Policy Law',
-                      style: context.textFontWeight600
-                          .onFontSize(context.resources.dimen.dp15),
-                    ),
-                    SizedBox(
-                      height: context.resources.dimen.dp5,
-                    ),
-                    Text(
-                      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text',
-                      style: context.textFontWeight400
-                          .onFontSize(context.resources.dimen.dp11),
-                    ),
-                  ],
-                ),
-              )
             ],
           ),
         ),
